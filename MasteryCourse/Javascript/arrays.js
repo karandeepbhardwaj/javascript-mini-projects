5dbd3482bcb0735424b7af2d943470b19c6221f5
Karandeep <karandiip@gmail.com>
2019-12-15 02:50:05 -0500

    learning object in javascript

--- MasteryCourse/Javascript/arrays.js
+++ MasteryCourse/Javascript/arrays.js
@@ -1,3 +1,30 @@
 var listOfAnimals = ["Tiger", "bear", "cat", "bear"];
 
-console.log(listOfAnimals[2]);
+var user1 = {
+
+    userName: "Karandeep",
+    passWord: "dontask",
+    age: 24,
+    technologies: ["Coding", "Basketball", "Music"],
+    isMarried: false,
+    shout: function() {
+        console.log("Do you want me to shout?");
+        console.log("Huhuhahahahahhaha");
+    }
+};
+
+var user2 = {
+
+    userName: "Jaya",
+    passWord: "kannu",
+    age: 23,
+    technologies: ["Vellapanti", "Dance", "Cooking"],
+    isMarried: false,
+    shout: function() {
+        console.log("Do you want me to shout?");
+        console.log("hihihihihihihihihihi");
+    }
+};
+
+
+var userList = [user1, user2];
