6c5c936a635a0d3fb7a0a35ceeab2b41cda462cb
Karandeep <karandiip@gmail.com>
2019-12-17 17:55:34 -0500

    Background generator

--- MasteryCourse/Javascript/DOM/script.js
+++ MasteryCourse/Javascript/DOM/script.js
@@ -6,36 +6,72 @@
 
 
 var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul")
+var deleteBtns = document.getElementsByClassName("delete");
+var items = ul.getElementsByTagName("li");
+
+
+
+//add event listener to first 6 btns in HTML file
+for (var i = 0; i < deleteBtns.length; i++) {
+    deleteBtns[i].addEventListener("click", removeParent, false);
+}
+
+
+//from StackOverflow:
+function removeParent(evt) {
+    evt.target.removeEventListener("click", removeParent, false);
+    evt.target.parentNode.remove();
+}
+
+//click on a list item and it strikethroughs the text
+function getEventTarget(e) {
+    e = e || window.event;
+    return e.target || e.srcElement;
+}
+
+
+ul.onclick = function(event) {
+    var target = getEventTarget(event);
+    target.classList.toggle("done");
+}
+
+
+//adding new items:
 
 function inputLength() {
     return input.value.length;
 }
 
-function createList() {
+function createListElement() {
+    var btn = document.createElement("button");
+    btn.innerHTML = "Delete";
+    btn.onclick = removeParent;
+
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
+    li.innerHTML = li.innerHTML + " ";
+    li.appendChild(btn);
+
     ul.appendChild(li);
     input.value = "";
 }
 
-function addListAfterClick() {
+
+function addToListAfterClick() {
     if (inputLength() > 0) {
-        createList();
+        createListElement();
     }
 }
 
-function addListAfterEnter(event) {
+function addToListAfterKeypress(event) {
     if (inputLength() > 0 && event.keyCode === 13) {
-        createList();
+        createListElement();
     }
 }
 
-function status(event) {
-    var li = event.querySelector;
-    console.log(li);
-}
-// button.addEventListener("click", addListAfterClick);
-// input.addEventListener("keypress", addListAfterEnter);
-ul.addEventListener("click", status)
+
+button.addEventListener("click", addToListAfterClick);
+
+input.addEventListener("keypress", addToListAfterKeypress);
